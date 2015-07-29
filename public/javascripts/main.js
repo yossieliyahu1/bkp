497625d153c377da248e2d481d1f2d3a9a5613f7
yossieliyahu <yossi@montiera.com>
2015-07-29 10:30:11 +0300

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+﻿
+
+(function () { 
+
+    //$(":file").filestyle({ buttonName: "btn-primary" });
+
+
+    $("#newPath2").change(function () {
+        var path = $(this).val();
+        $("#pathList").append("<li class='list-group-item'>" + path + "</li>");
+        $(this).val("");
+    });
+
+})();
